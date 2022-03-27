b99726f00a805cbb1109743dc60d815ffc73286d
Leonardo <barinas.code@gmail.com>
2022-03-27 13:44:25 -0400

    updated recovery request message

--- resources/views/emails/recovery-request.blade.php
+++ resources/views/emails/recovery-request.blade.php
@@ -6,7 +6,11 @@
  <body>
     <h1>Restablecer cuenta</h1>
     <p>Fecha: {{date('d/m/y H:i:s')}}</p>
-    <p>Hola {{$name}} has solicitado recuperar tu cuenta, usa este codigo para recupearla</p>
+    <p>Hola {{$name}}:</p>
+    <p>Hemos recibido una solicitud de restablecimiento de contraseña de tu cuenta</p>
+    <p>Usa el codigo que aparece a continuación para cambiar tu contraseña</p>
+    <p>Ten en cuenta que este codigo es válido solo durante 24 horas o una vez usado. Una vez transcurrido el plazo, deberás volver a solicitar el restablecimiento de la contraseña. </p>
+    
     <span>Codigo de recuperacion:</span>
     <h1>{{ $token }}</h1>
     <br/>
